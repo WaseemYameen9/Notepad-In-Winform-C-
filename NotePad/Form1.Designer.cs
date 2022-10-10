cd475ed4578dd4bfaee45fd903a3e98650cd00cd
waseem <wyameen152@gmail.com>
2022-10-10 20:55:26 +0500

    First Commit

---
+++ NotePad/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace NotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWriteParagraph = new System.Windows.Forms.TextBox();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.cboFamily = new System.Windows.Forms.ComboBox();
+            this.cboFonts = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtWriteParagraph
+            // 
+            this.txtWriteParagraph.Location = new System.Drawing.Point(12, 64);
+            this.txtWriteParagraph.Multiline = true;
+            this.txtWriteParagraph.Name = "txtWriteParagraph";
+            this.txtWriteParagraph.Size = new System.Drawing.Size(776, 374);
+            this.txtWriteParagraph.TabIndex = 0;
+            this.txtWriteParagraph.TextChanged += new System.EventHandler(this.txtWriteParagraph_TextChanged);
+            // 
+            // btnBold
+            // 
+            this.btnBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBold.Location = new System.Drawing.Point(25, 27);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(23, 31);
+            this.btnBold.TabIndex = 1;
+            this.btnBold.Text = "B";
+            this.btnBold.UseVisualStyleBackColor = true;
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnItalic.ForeColor = System.Drawing.Color.Black;
+            this.btnItalic.Location = new System.Drawing.Point(54, 27);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(23, 31);
+            this.btnItalic.TabIndex = 3;
+            this.btnItalic.Text = "I";
+            this.btnItalic.UseVisualStyleBackColor = true;
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            // 
+            // cboFamily
+            // 
+            this.cboFamily.FormattingEnabled = true;
+            this.cboFamily.Location = new System.Drawing.Point(83, 33);
+            this.cboFamily.Name = "cboFamily";
+            this.cboFamily.Size = new System.Drawing.Size(121, 21);
+            this.cboFamily.TabIndex = 4;
+            this.cboFamily.SelectedValueChanged += new System.EventHandler(this.cboFamily_SelectedValueChanged);
+            // 
+            // cboFonts
+            // 
+            this.cboFonts.FormattingEnabled = true;
+            this.cboFonts.Location = new System.Drawing.Point(225, 33);
+            this.cboFonts.Name = "cboFonts";
+            this.cboFonts.Size = new System.Drawing.Size(42, 21);
+            this.cboFonts.TabIndex = 5;
+            this.cboFonts.SelectedValueChanged += new System.EventHandler(this.cboFonts_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboFonts);
+            this.Controls.Add(this.cboFamily);
+            this.Controls.Add(this.btnItalic);
+            this.Controls.Add(this.btnBold);
+            this.Controls.Add(this.txtWriteParagraph);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "NotePad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWriteParagraph;
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.Button btnItalic;
+        private System.Windows.Forms.ComboBox cboFamily;
+        private System.Windows.Forms.ComboBox cboFonts;
+    }
+}
+
---
+++ NotePad/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotePad
+{
+    public partial class Form1 : Form
+    {
+        bool isBold=false;
+        bool isItalic = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<int> fontsize = new List<int>();
+            for(int i=8; i < 30;i+=2)
+            {
+                fontsize.Add(i);
+            }
+            fontsize.Add(36);
+            fontsize.Add(48);
+            fontsize.Add(72);
+            cboFonts.DataSource = fontsize;
+            cboFonts.SelectedItem = fontsize[2];
+            cboFamily.DataSource = FontFamily.Families;
+            cboFamily.DisplayMember="Name";
+            cboFamily.SelectedItem = FontFamily.Families[0];
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            if(! isBold)
+            {
+                txtWriteParagraph.Font = new Font(txtWriteParagraph.Font,FontStyle.Bold);
+            }
+            if(isBold)
+            {
+                txtWriteParagraph.Font = new Font(txtWriteParagraph.Font, FontStyle.Regular);
+            }
+            isBold = !isBold;
+        }
+        private void txtWriteParagraph_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            if (!isItalic)
+            {
+                txtWriteParagraph.Font = new Font(txtWriteParagraph.Font, FontStyle.Italic);
+            }
+            if (isItalic)
+            {
+                txtWriteParagraph.Font = new Font(txtWriteParagraph.Font, FontStyle.Regular);
+            }
+            isItalic = !isItalic;
+        }
+        private void cboFamily_SelectedValueChanged(object sender, EventArgs e)
+        {
+            Font myfont = new Font(cboFamily.Text, int.Parse(cboFonts.Text));
+            txtWriteParagraph.Font = myfont;
+        }
+
+        private void cboFonts_SelectedValueChanged(object sender, EventArgs e)
+        {
+            txtWriteParagraph.Font = new Font(txtWriteParagraph.Font.FontFamily, int.Parse(cboFonts.SelectedItem.ToString()), txtWriteParagraph.Font.Style);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path = "D:\\2nd Semester\\OOP\\NotePad\\NotePad Data.txt";
+            StreamWriter file = new StreamWriter(path);
+            file.Write(txtWriteParagraph.Text);
+            file.Close();
+        }
+    }
+}
